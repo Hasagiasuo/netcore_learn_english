2a1eeeb3aac87e8cc804c415ec364b9040648adc
aphsol <hasagiasuo835@gmail.com>
2024-09-07 11:07:41 +0300

    a

--- telebot/obj/Debug/net8.0/telebot.AssemblyInfo.cs
+++ telebot/obj/Debug/net8.0/telebot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("telebot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+389d2089bac7e8d064cad9bdbc5ca304981b8b16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8bd764011c31bfa6d48787a28b98696a968ef6")]
 [assembly: System.Reflection.AssemblyProductAttribute("telebot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("telebot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- web/obj/Debug/net8.0/web.AssemblyInfo.cs
+++ web/obj/Debug/net8.0/web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+389d2089bac7e8d064cad9bdbc5ca304981b8b16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8bd764011c31bfa6d48787a28b98696a968ef6")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
