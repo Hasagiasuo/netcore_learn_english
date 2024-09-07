f5cb42ccd02c1d65248ccbe6767e346a4ad58bf2
dungeon master <vb2564720@gmail.com>
2024-09-07 09:09:54 +0300

    l123

--- web/obj/Release/net8.0/web.AssemblyInfo.cs
+++ web/obj/Release/net8.0/web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f1bdd08caaad1e2718511c52f574791fb92a98f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33aecb2d9b88eca2139f3460c53005d482a41a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
