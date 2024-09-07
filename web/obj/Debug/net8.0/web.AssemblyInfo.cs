606cefb615204407d5bc362d4924116cf2851a81
Gavixx <rzhvanskoy@gmail.com>
2024-09-07 09:53:10 +0300

    312

--- web/obj/Debug/net8.0/web.AssemblyInfo.cs
+++ web/obj/Debug/net8.0/web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ab69370ad1e7e9972b6a493d2c21cfae39fcd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2f420292f4b0de087aab26033d5b673f30b2b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
