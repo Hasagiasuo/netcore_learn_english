176f2885bcf3e8eaccecc1b123a18a673f849521
aphsol <hasagiasuo835@gmail.com>
2024-09-07 09:35:39 +0300

    bot

--- web/obj/Debug/net8.0/web.AssemblyInfo.cs
+++ web/obj/Debug/net8.0/web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+675d164efaea4c4d15e435457def9e431abe8889")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0f218093be68268a3f8019a2e9c203fd42265f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
